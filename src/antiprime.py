66753bf7213bf66a8e3b394edbb4d0e76b992cc7
Francina Pons <francina.pons01@estudiant.upf.edu>
2024-10-22 00:39:27 +0200

    canvis2

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,50 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main(x) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+	c1 = 0 
+	i = 1
 
+	while i <= x:
+		if x % i == 0:
+			c1 = c1 + 1
+		i = i + 1
+	j = x - 1
+	c2 = 0
+
+	while j >= 1 and c2 < c1:
+		l = 1
+		c2 = 0
+		
+		if j % l == 0:
+			c2 = c2 + 1
+
+		l = l + 1
+	j = j - 1
+
+	if c1 <= c2:
+		res = 'not anti-prime'
+
+	else: 
+		res = 'anti-prime'
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	return(res)
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	import sys 
+	x = int(sys.argv[1])
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+	print(main(x))
